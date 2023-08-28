af4d3770f04c791ff5b5456057c95c6abdc15a6e
EdhalaKarthik <karthikedhala3@gmail.com>
2023-08-28 22:14:56 +0530

    commiting server side rendering page code

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,25 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import { useEffect, useState } from 'react'
+
+
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export async function getServerSideProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await res.json();
+  return {
+    props: {
+      todos: data
+    }
+  }
+}
+
+
+export default function Home({ todos }) {
+
   return (
     <>
       <Head>
@@ -14,6 +29,22 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      {
+        todos?.length === 0 ? (
+          <div>Loading...</div>
+        ) : (
+          todos?.map(e => (
+            <div key={e.id}>
+              <p>
+                {e.id}:{e.title}
+              </p>
+            </div>
+          ))
+        )
+      }
+
+
       <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.description}>
           <p>
